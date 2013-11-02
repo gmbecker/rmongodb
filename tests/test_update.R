8b3a532628aaf838e3e807170d54eb56de160f39
Dr. rer. nat. Markus Schmidberger <markus.schmidberger@googlemail.com>
2013-11-02 21:36:20 +0100

    RUnit added to test_update

--- tests/test_update.R
+++ tests/test_update.R
@@ -1,8 +1,31 @@
 library(rmongodb)
 library(RUnit)
 
+# empty test db
+mongo <- mongo.create()
+db <- "rmongodb"
+ns <- paste(db, "test_update", sep=".")
+mongo.drop(mongo, ns)
+
+# inster data
 buf <- mongo.bson.buffer.create()
-mongo.bson.buffer.append(buf, "_id", oid)
+mongo.bson.buffer.append(buf, "name", "Dave")
+mongo.bson.buffer.append(buf, "age", 27L)
+x <- mongo.bson.from.buffer(buf)
+buf <- mongo.bson.buffer.create()
+mongo.bson.buffer.append(buf, "name", "Fred")
+mongo.bson.buffer.append(buf, "age", 31L)
+y <- mongo.bson.from.buffer(buf)
+buf <- mongo.bson.buffer.create()
+mongo.bson.buffer.append(buf, "name", "Silvia")
+mongo.bson.buffer.append(buf, "age", 24L)
+z <- mongo.bson.from.buffer(buf)
+mongo.insert.batch(mongo, ns, list(x, y, z))
+
+
+# update one document
+buf <- mongo.bson.buffer.create()
+mongo.bson.buffer.append(buf, "name", "Silvia")
 query <- mongo.bson.from.buffer(buf)
 
 buf <- mongo.bson.buffer.create()
@@ -12,3 +35,8 @@ mongo.bson.buffer.finish.object(buf)
 op  <- mongo.bson.from.buffer(buf)
 
 mongo.update(mongo, ns, query, op)
+
+
+checkEquals(
+    mongo.bson.value( mongo.find.one(mongo, ns, query), "age"),
+    mongo.bson.value( z, "age") +1 )
