f752e7c47c217255c61dc4d899185dc742895e8a
Dr. rer. nat. Markus Schmidberger <markus.schmidberger@googlemail.com>
2013-11-02 21:56:40 +0100

    test_inserts finalized

--- tests/test_insert.R
+++ tests/test_insert.R
@@ -1,56 +1,62 @@
 library(rmongodb)
 library(RUnit)
 
+# empty test db
+mongo <- mongo.create()
+db <- "rmongodb"
+ns <- paste(db, "test_insert", sep=".")
+mongo.drop(mongo, ns)
+
+# insert data
 buf <- mongo.bson.buffer.create()
 mongo.bson.buffer.append(buf, "name", "John")
 mongo.bson.buffer.append(buf, "age", 22L)
 x <- mongo.bson.from.buffer(buf)
 mongo.insert(mongo, ns, x);
 
+checkEquals(mongo.count(mongo, ns), 1)
+checkEquals( 
+  mongo.bson.value(mongo.find.one(mongo, ns, query=list("name"="John")), "name"),
+  "John")
+
+
 buf <- mongo.bson.buffer.create()
 mongo.bson.buffer.append(buf, "name", "John")
 mongo.bson.buffer.append(buf, "age", 27L)
-x <- mongo.bson.from.buffer(buf)
-mongo.insert(mongo, ns, x);
+y <- mongo.bson.from.buffer(buf)
+mongo.insert(mongo, ns, y);
 
+checkEquals(mongo.count(mongo, ns), 2)
+checkEquals( 
+  mongo.bson.value(mongo.find.one(mongo, ns, query=list("name"="John", "age"=27L)), "age"),
+  27)
 
-
-print("insert")
-#print(mongo.insert(mongo, ns, b))  #
 
 buf <- mongo.bson.buffer.create()
 mongo.bson.buffer.append(buf, "name", "Dwight")
 mongo.bson.buffer.append(buf, "city", "NY")
-b <- mongo.bson.from.buffer(buf)
-mongo.insert(mongo, ns, b)
+z <- mongo.bson.from.buffer(buf)
+mongo.insert(mongo, ns, z)
 
-buf <- mongo.bson.buffer.create()
-mongo.bson.buffer.append(buf, "name", "Dave")
-mongo.bson.buffer.append(buf, "city", "Cincinnati")
-x <- mongo.bson.from.buffer(buf)
+checkEquals(mongo.count(mongo, ns), 3)
+checkEquals( 
+  mongo.bson.value(mongo.find.one(mongo, ns, query=list("name"="Dwight")), "name"),
+  "Dwight")
 
-buf <- mongo.bson.buffer.create()
-mongo.bson.buffer.append(buf, "name", "Fred")
-mongo.bson.buffer.append(buf, "city", "Dayton")
-mongo.bson.buffer.append(buf, "age", 21L)
-print(mongo.bson.buffer.size(buf))
 
-y <- mongo.bson.from.buffer(buf)
-print(mongo.bson.size(y))
-
-l <- mongo.bson.to.list(y)
-print(l$name)
-print(l$city)
-l$city <- "Detroit"
-print(l)
-
+# insert batch
 buf <- mongo.bson.buffer.create()
 mongo.bson.buffer.append(buf, "name", "Silvia")
 mongo.bson.buffer.append(buf, "city", "Cincinnati")
-z <- mongo.bson.from.buffer(buf)
-mongo.insert.batch(mongo, ns, list(x, y, z))
+a <- mongo.bson.from.buffer(buf)
+mongo.insert.batch(mongo, ns, list(x, y, z, a))
 
-print("index create x2")
-print(mongo.index.create(mongo, ns, "city"))
+checkEquals(mongo.count(mongo, ns), 7)
 
-print(mongo.index.create(mongo, ns, c("name", "city")))
+
+# create index
+mongo.index.create(mongo, ns, "city")
+mongo.index.create(mongo, ns, c("name", "city"))
+
+# functionality for checks is missing
+# ToDo
