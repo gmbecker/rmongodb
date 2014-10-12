530f3e00ade8d7be50d91464aec76d004fc3a40a
Dmitriy Selivanov <selivanov.dmitriy@gmail.com>
2014-10-12 21:38:00 +0400

    create utils file for helper functions

--- R/mongo_high_level.R
+++ R/mongo_high_level.R
@@ -1,19 +1,3 @@
-mongo.parse.ns <- function(ns)
-{
-  pos <- regexpr('\\.', ns)
-  if (pos == 0 || pos == -1) {
-    warning("mongo.parse.ns: No '.' in namespace")
-    return(NULL)
-  } else {
-    db <- substr(ns, 1, pos-1)
-    collection <- substr(ns, pos+1, nchar(ns))
-    return(list(db=db, collection=collection))
-  }
-}
-
-
-
-
 #' Get a vector of distinct values for keys in a collection
 #' 
 #' Get a vector of distinct values for keys in a collection.
