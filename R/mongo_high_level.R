92955c4b51b9954b8b96c9e46347c3a925eb532d
Dmitriy Selivanov <selivanov.dmitriy@gmail.com>
2014-11-02 19:07:52 +0300

    small fixes to pass "R CMD check --as-cran"

--- R/mongo_high_level.R
+++ R/mongo_high_level.R
@@ -77,8 +77,9 @@ mongo.get.values <- mongo.distinct
 #' Alternately, \code{pipeline} may be a \link{list} of valid JSON \link{character} strings which will be converted to a
 #' mongo.bson object by \code{\link{mongo.bson.from.JSON}()}.
 #' @param explain (\link{logical}) Optional, MongoDB 2.6+. Specifies to return the information on the processing of the pipeline. References above.
-#' @param (\link{logical}) Optional, MongoDB 2.6+. Enables writing to temporary files. When set to true, aggregation stages can write data to the _tmp subdirectory in the dbPath directory.
+#' @param allowDiskUse (\link{logical}) Optional, MongoDB 2.6+. Enables writing to temporary files. When set to true, aggregation stages can write data to the _tmp subdirectory in the dbPath directory.
 #' @param cursor (\link{mongo.bson}) Optional, MongoDB 2.6+. Specify a document that contains options that control the creation of the cursor object.
+#' @param ... Arguments to be passed to methods, such as \link{mongo.bson.to.list}, \link{fromJSON}
 #' Unfortunately, current underlying mongo-c-driver can return BSON from aggreagation camand. Cursors are not supported.
 #'
 #' Alternately, \code{cursor} may be a list which will be converted to a
